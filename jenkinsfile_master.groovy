2378053265c08b891e955761b55fb083d8c5140a
sisul1204 <lizhipeng1021@163.com>
2020-10-25 08:56:40 +0800

    修改mvn 和 java

--- jenkinsfile_master.groovy
+++ jenkinsfile_master.groovy
@@ -10,8 +10,8 @@ stage('maven compile & package') {
         sh ". ~/.bash_profile"
 
         //定义maven java环境
-        def mvnHome = tool 'maven-3.6.0_master'
-        def jdkHome = tool 'jdk1.8_master'
+        def mvnHome = tool '/usr/local/apache-maven-3.6.3'
+        def jdkHome = tool '/usr/bin/java'
         env.PATH = "${mvnHome}/bin:${env.PATH}"
         env.PATH = "${jdkHome}/bin:${env.PATH}"
         sh "mvn clean install"
